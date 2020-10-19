7df7a33186f235c49c50ade511b1ce5be50d2795
tho.nguyentien <tho.nguyentien@vndirect.com.vn>
2020-10-19 17:26:35 +0700

    first commit

---
+++ src/main/java/nguyentientho/spring_bk/SpringBkApplication.java
@@ -0,0 +1,13 @@
+package nguyentientho.spring_bk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nguyentientho/spring_bk/SpringBkApplicationTests.java
@@ -0,0 +1,13 @@
+package nguyentientho.spring_bk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
